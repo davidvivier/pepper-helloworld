721082a6b60a4e0f081eaec8abca19901e8913da
David Vivier <david.vivier@cpe.fr>
2018-11-20 16:14:31 +0100

    Tend la main et demande mouchoir

--- appli/mouchoir/mouchoir.pml
+++ appli/mouchoir/mouchoir.pml
@@ -4,12 +4,17 @@
     <BehaviorDescriptions>
         <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
     </BehaviorDescriptions>
-    <Dialogs />
+    <Dialogs>
+        <Dialog name="ExampleDialog" src="behavior_1/ExampleDialog/ExampleDialog.dlg" />
+        <Dialog name="DemandeMouchoirTopic" src="DemandeMouchoirTopic/DemandeMouchoirTopic.dlg" />
+    </Dialogs>
     <Resources />
-    <Topics />
-    <IgnoredPaths>
-        <Path src="translations/translation_en_US.ts" />
-    </IgnoredPaths>
+    <Topics>
+        <Topic name="ExampleDialog_enu" src="behavior_1/ExampleDialog/ExampleDialog_enu.top" topicName="ExampleDialog" language="en_US" />
+        <Topic name="ExampleDialog_frf" src="behavior_1/ExampleDialog/ExampleDialog_frf.top" topicName="ExampleDialog" language="fr_FR" />
+        <Topic name="DemandeMouchoirTopic_frf" src="DemandeMouchoirTopic/DemandeMouchoirTopic_frf.top" topicName="DemandeMouchoirTopic" language="fr_FR" />
+    </Topics>
+    <IgnoredPaths />
     <Translations auto-fill="fr_FR">
         <Translation name="translation_fr_FR" src="translations/translation_fr_FR.ts" language="fr_FR" />
     </Translations>
--- appli/mouchoir/translations/translation_fr_FR.ts
+++ appli/mouchoir/translations/translation_fr_FR.ts
@@ -2,6 +2,29 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR">
     <context>
+        <name>behavior_1/behavior.xar:/Demander mouchoir</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>J'ai besoin d'un mouchoir, s'il vous plaît </source>
+            <comment>Text</comment>
+            <translation type="unfinished">J'ai besoin d'un mouchoir, s'il vous plaît </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Dire merci</name>
+        <message>
+            <source>Merci</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Merci</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Merci beaucoup !</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Merci beaucoup !</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
             <source>Bonjour</source>
@@ -9,10 +32,17 @@
             <translation type="vanished">Bonjour</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>J'ai besoin d'un mouchoir, s'il vous plaît </source>
             <comment>Text</comment>
-            <translation type="unfinished">J'ai besoin d'un mouchoir, s'il vous plaît </translation>
+            <translation type="obsolete">J'ai besoin d'un mouchoir, s'il vous plaît </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Merci</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Merci</translation>
         </message>
     </context>
 </TS>
