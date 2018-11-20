c67f449349afea52c991f0d4081d7b5576e89fb3
David Vivier <david.vivier@cpe.fr>
2018-11-20 14:45:58 +0100

    Begin appli mouchoir

---
+++ appli/mouchoir/mouchoir.pml
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="mouchoir" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources />
+    <Topics />
+    <IgnoredPaths>
+        <Path src="translations/translation_en_US.ts" />
+    </IgnoredPaths>
+    <Translations auto-fill="fr_FR">
+        <Translation name="translation_fr_FR" src="translations/translation_fr_FR.ts" language="fr_FR" />
+    </Translations>
+</Package>
---
+++ appli/mouchoir/translations/translation_fr_FR.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Bonjour</source>
+            <comment>Text</comment>
+            <translation type="vanished">Bonjour</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>J'ai besoin d'un mouchoir, s'il vous plaît </source>
+            <comment>Text</comment>
+            <translation type="unfinished">J'ai besoin d'un mouchoir, s'il vous plaît </translation>
+        </message>
+    </context>
+</TS>
